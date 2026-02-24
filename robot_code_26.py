d2649f8e61bbe79a2b18b9855d87078bc7c997f9
josif9 <160293929+josif9@users.noreply.github.com>
2026-02-23 17:04:52 -0800

    Refactor robot code to use rev library for motors
    The new, improved, and working robot code.

--- robot_code_26.py
+++ robot_code_26.py
@@ -1,62 +1,64 @@
 # TODO: insert robot code here
+import rev
 import wpilib
+from cscore import CameraServer
 from wpilib.drive import DifferentialDrive
-from rev import SparkMax, SparkLowLevel
 
-'''
-leftFrontMotor = 0
-rightFrontMotor = 1
-leftRearMotor = 2
-rightRearMotor = 3
-intakeMotor = 4
-shooterMotor = 5
-'''
+CameraServer.startAutomaticCapture(0)
 
 class MyRobot(wpilib.TimedRobot):
-    
-    global leftFrontMotor,rightFrontMotor,leftRearMotor,rightRearMotor,intakeMotor,shooterMotor
     
     def robotInit(self):
         
         # Motor controller setup
-        self.leftFront = SparkMax(1, SparkLowLevel.MotorType.kBrushed)
-        self.rightFront = SparkMax(2, SparkLowLevel.MotorType.kBrushed)
-        self.leftRear = SparkMax(3, SparkLowLevel.MotorType.kBrushed)
-        self.rightRear = SparkMax(4, SparkLowLevel.MotorType.kBrushed)
-        self.intake = SparkMax(6, SparkLowLevel.MotorType.kBrushed)
-        self.shooter = SparkMax(5, SparkLowLevel.MotorType.kBrushed)
+        self.leftFront = rev.SparkMax(1, rev.SparkLowLevel.MotorType.kBrushed)
+        self.rightFront = rev.SparkMax(2, rev.SparkLowLevel.MotorType.kBrushed)
+        self.leftRear = rev.SparkMax(3, rev.SparkLowLevel.MotorType.kBrushed)
+        self.rightRear = rev.SparkMax(4, rev.SparkLowLevel.MotorType.kBrushed)
+        self.dualMotor = rev.SparkMax(6, rev.SparkLowLevel.MotorType.kBrushed)
+        self.singleMotor = rev.SparkMax(5, rev.SparkLowLevel.MotorType.kBrushed)
         
+        # Creating motor groups to differentiate the left and right side motors
+        self.leftMotorGroup = wpilib.MotorControllerGroup(self.leftFront, self.leftRear)
+        self.rightMotorGroup = wpilib.MotorControllerGroup(self.rightFront, self.rightRear)
+                                                          
         # Tank drive setup
-        self.drive = DifferentialDrive(self.leftFront, self.leftRear, self.rightFront, self.rightRear)
+        self.drive = DifferentialDrive(self.leftMotorGroup, self.rightMotorGroup)
         
         # Controller setup 
         self.controller = wpilib.XboxController(0)
         
     def teleopPeriodic(self):
         
-        left_y = -self.controller.getLeftY()
+        # Gets the values from the different controller inputs
+        left_y = self.controller.getLeftY()
         right_y = -self.controller.getRightY()
         rightTrigger = self.controller.getRightTriggerAxis()
+        leftTrigger = self.controller.getLeftTriggerAxis()
         
-        
-        if left_y and right_y < 0.1:
+        # This segment contains the staements that tell the robot how fast and in what direction to drive
+        if abs(left_y) < 0.1:
             left_speed = 0
-            right_speed = 0
         else:
             left_speed = left_y
+        
+        if abs(right_y) < 0.1:
+            right_speed = 0
+        else:
             right_speed = right_y
-
+            
         self.drive.tankDrive(left_speed, right_speed)
         
+        # Segment for the functions of the triggers
         if (rightTrigger > 0.2):
-            self.shooter.set(0.5)
+            self.singleMotor.set(-1)
+            self.dualMotor.set(-1)
+        elif(leftTrigger > 0.2):
+            self.singleMotor.set(1)
+            self.dualMotor.set(-0.5)
         else:
-            self.shooter.set(0)
-            
-        if (self.intake > 0.2):
-            self.intake.set(0.5)
-        else:
-            self.intake.set(0)
+            self.singleMotor.set(0)
+            self.dualMotor.set(0)
             
 if __name__ == "__main__":
     wpilib.run(MyRobot)
